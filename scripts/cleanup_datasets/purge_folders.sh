2924a966c6e83cc1f3e1e62d4e881d754160cbef
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2022-01-13 16:27:21 +0100

    check not already running

--- scripts/cleanup_datasets/purge_folders.sh
+++ scripts/cleanup_datasets/purge_folders.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+ME=`basename "$0"`
+if /sbin/pidof -o %PPID -x "$ME">/dev/null; then
+    echo "Script $ME already running, skipping"
+    exit 0
+fi
+
+
 cd "$(dirname "$0")"/../..
 python ./scripts/cleanup_datasets/cleanup_datasets.py -d 60 -5 -r "$@" >> ./scripts/cleanup_datasets/purge_folders.log
